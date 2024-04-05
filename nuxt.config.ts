88e575006de01bf8df0c2353b477ab2a8b1bd233
snake-git <one2three531@126.com>
2024-04-05 10:10:45 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,59 +1,61 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-    devtools: {enabled: false},
-    modules: ['@nuxt/ui', '@nuxtjs/i18n'],
-    css: ['~/assets/css/style.css'],
-    devServer: {
-        port: 3001,
-    },
-    routeRules: {
-        '/': {
-            prerender: true,
-        }
-    },
-    app: {
-        head: {
-            title: 'CF AI Web',
-            meta: [
-                {
-                    name: 'keywords',
-                    content: 'CF AI Web, AI, Cloudflare Workers, ChatGPT, GeminiPro, Google Generative AI'
-                },
-                {
-                    name: 'description',
-                    content: 'Integrated web platform supporting GeminiPro/Cloudflare Workers AI/ChatGPT by Jazee6'
-                }
-            ],
-            link: [
-                {
-                    rel: 'manifest',
-                    href: '/manifest.json'
-                }
-            ]
-        }
-    },
-    vite: {
-        build: {
-            rollupOptions: {
-                output: {
-                    manualChunks: (id) => {
-                        if (id.includes('node_modules')) {
-                            return 'vendor'
-                        }
-                    }
-                }
-            }
-        }
-    },
-    i18n: {
-        vueI18n: './i18n.config.ts',
-        strategy: 'no_prefix',
-        defaultLocale: 'zh',
+  devtools: { enabled: false },
+  modules: ['@nuxt/ui', '@nuxtjs/i18n'],
+  css: ['~/assets/css/style.css'],
+  devServer: {
+    port: 3001,
+  },
+  routeRules: {
+    '/': {
+      prerender: true,
     }
-     nitro: {
-         vercel: {
-             regions: ["cle1", "iad1", "pdx1", "sfo1", "sin1", "syd1", "hnd1", "kix1"]
-         }
-     }
+  },
+  app: {
+    head: {
+      title: 'CF AI Web',
+      meta: [
+        {
+          name: 'keywords',
+          content: 'CF AI Web, AI, Cloudflare Workers, ChatGPT, GeminiPro, Google Generative AI'
+        },
+        {
+          name: 'description',
+          content: 'Integrated web platform supporting GeminiPro/Cloudflare Workers AI/ChatGPT by Jazee6'
+        }
+      ],
+      link: [
+        {
+          rel: 'manifest',
+          href: '/manifest.json'
+        }
+      ]
+    }
+  },
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: (id) => {
+            if (id.includes('node_modules')) {
+              return 'vendor'
+            }
+          }
+        }
+      }
+    }
+  },
+  i18n: {
+    vueI18n: './i18n.config.ts',
+    strategy: 'no_prefix',
+    defaultLocale: 'zh',
+  },
+  build: {
+    nitro: {
+      vercel: {
+        regions: ["cle1", "iad1", "pdx1", "sfo1", "sin1", "syd1", "hnd1", "kix1"]
+      }
+    }
+  }
 })
